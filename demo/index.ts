344bdc582750f1bb4370436193480ce7bcd74dfa
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-02-28 13:49:39 +0900

    feat: improve demo

--- demo/index.ts
+++ demo/index.ts
@@ -3,24 +3,46 @@ import bakedData from './action.json'
 
 console.log(bakedData)
 
-const player = new Player('target', {
+let player = new Player('target', {
   bakedData: bakedData as any,
   width: '100%',
   height: '100%',
 })
 
-document.getElementById('dispose')?.addEventListener('click', async () => {
+document.getElementById('dispose')?.addEventListener('click', () => {
   player.dispose()
 })
-document.getElementById('reverse')?.addEventListener('click', async () => {
+document.getElementById('reverse')?.addEventListener('click', () => {
   player.reverse()
 })
-document.getElementById('play')?.addEventListener('click', async () => {
+document.getElementById('play')?.addEventListener('click', () => {
   player.play()
 })
-document.getElementById('pause')?.addEventListener('click', async () => {
+document.getElementById('pause')?.addEventListener('click', () => {
   player.pause()
 })
-document.getElementById('stop')?.addEventListener('click', async () => {
+document.getElementById('stop')?.addEventListener('click', () => {
   player.stop()
+})
+
+document.getElementById('file')?.addEventListener('input', (e) => {
+  const target = e.target as any
+  if (target?.files?.length !== 1) return
+
+  const file = target.files[0]
+  const reader = new FileReader()
+  reader.onload = () => {
+    const jsonStr = reader.result as string
+    if (player) {
+      player.dispose()
+    }
+    const bakedData = JSON.parse(jsonStr)
+    player = new Player('target', {
+      bakedData: bakedData as any,
+      width: '100%',
+      height: '100%',
+    })
+  }
+  reader.onerror = () => alert('Invalid data')
+  reader.readAsText(file)
 })
