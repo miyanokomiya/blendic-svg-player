c351d657bf0bc821738dc44e06ff67d9906412f4
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-06-02 08:56:17 +0900

    update

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,17 +1,44 @@
-<svelte:options tag="blendic-svg-player" immutable="{true}"/>
+<svelte:options tag="blendic-svg-player" immutable={true} />
 
 <script lang="ts">
-export let name = 'a'
+  import type { AnimationLoop } from './animationLoop'
+
+  interface ElementNode {
+    id: string
+    tag: string
+    attributes: { [name: string]: string }
+    children: (ElementNode | string)[]
+  }
+
+  interface Action {
+    name: string
+    attributesMapPerFrame: {
+      [elementId: string]: { [name: string]: string }
+    }[]
+  }
+
+  interface BakedData {
+    version: string
+    appVersion: string
+    actions: Action[]
+    svgTree: ElementNode
+  }
+
+  export let name = 'a'
+
+  export let bakedData: BakedData
+  export let width: number | string = 0
+  export let height: number | string = 0
+
+  let animationLoop: AnimationLoop | undefined
+  let currentFrame = 0
+  let reversed = false
+  let oneshot = false
+  let currentActionName = ''
+  let actionsByName: { [name: string]: Action } = {}
 </script>
 
 <main>
   <h1>Hello Typescript!</h1>
   <p>{name}</p>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-</style>
--- src/animationLoop.ts
+++ src/animationLoop.ts
@@ -34,3 +34,4 @@ export function useAnimationLoop(
     },
   }
 }
+export type AnimationLoop = ReturnType<typeof useAnimationLoop>
